2909e9f8cb9f71d9baacf6c88f5c4626bc851131
Faijan-akhtar <Faijanakhtar20@gmail.com>
2024-10-18 09:49:35 -0600

    signature add

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import sing1 from '../assets/sing1.png'
 const Note = () => {
   return (
     <>
        <section className='mt-10'>
-              
-                <h3 className='font-bold text-green-600'>Your Vision <span className='text-yellow-500'>Our passion</span></h3>
+              <div className='flex justify-between'>
+              <h3 className='font-bold text-green-600'>Your Vision <span className='text-yellow-500'>Our passion</span></h3>
+              <img  className='h-7 ' src={sing1} alt='sing'/>
+              </div>
               
               
               </section>
